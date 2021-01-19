3e5a12b4f2eb84e1eca46f6b5cb7dad2c96bf4a2
Kleber Bastos <bastoskp@hotmail.com>
2021-01-19 17:26:55 -0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length
 
-  get(pos) {}
+  get(pos) {
+    if (this.length >= pos){
+      return this.items[pos]
+    } else {
+      throw new Error("outOfBounds")
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[0]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }else{
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
